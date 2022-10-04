9c94ed0ee86fd979c65bfa546969ea91cd74bf90
Ashley Beal <ashleybeal86@yahoo.com>
2022-10-04 19:33:34 -0400

    few small file changes

--- frontend/src/Error404.js
+++ frontend/src/Error404.js
@@ -2,10 +2,10 @@ function Error404() {
   return (
     <main>
       <h1>404: PAGE NOT FOUND</h1>
-      <p>Oops, sorry, we can't find this page!</p>
+      <p>Oops, sorry, we can't find this page!  (I probably just messed up my code, so please try again later)</p>
       <img
         src="http://localhost:5000/images/kittykat.jpeg"
-        alt="cute cat pic"
+        alt="cute cat pic since we can't show you what you REALLY want to see"
       />
     </main>
   );
